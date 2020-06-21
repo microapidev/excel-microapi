b79fb20110e590ff9e2dada23d4c11611eb6bd95
Eni. C <cornzyblack@users.noreply.github.com>
2020-06-21 18:58:28 +0100

    Update parser file eith recent changes

--- excel_api/views.py
+++ excel_api/views.py
@@ -43,26 +43,15 @@ class FilesAdd(generics.CreateAPIView):
 
 @api_view(['POST'])
 def parserview(request):
-    start_time = start_timer()
+    file_obj = request.data.get('content')
+    title = get_file_name(file_obj)
+    result = parse_excel_file(file_obj)
+    if result.get('data') is not None:
+        file = Files.objects.create(title=title, content=file_obj)
+        file.save()
 
-    title = get_file_name(request.data.get('content'))
-    file = Files.objects.get(title=title)
-    content = file.content.url
-    filepath = os.path.join(os.path.dirname(os.path.realpath(__file__)), content)
-    workbook = xlrd.open_workbook("."+filepath)
-    worksheet = workbook.sheet_by_name('Sheet1')
-    data = []
-    keys = [v.value for v in worksheet.row(0)]
-    for row_number in range(worksheet.nrows):
-        if row_number == 0:
-            continue
-        row_data = {}
-        for col_number, cell in enumerate(worksheet.row(row_number)):
-            row_data[keys[col_number]] = cell.value
-        data.append(row_data)
-    end_time = start_timer()
-    total_time = round(end_time - start_time, 2)
-    json_parsed = {'data': data, 'process_time': total_time}
+    json_parsed = result
+    return JsonResponse(json_parsed)
 
 
     return Response(json_parsed)
